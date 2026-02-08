12d617f7ec3ce4b09196f31e667913597b0de644
Blessy925 <malladiblessy@gmail.com>
2026-02-08 19:51:32 +0530

    Add files via upload

---
+++ DSA_arrayys.c
@@ -0,0 +1,45 @@
+#include <stdlib.h>
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+
+struct ListNode* addTwoNumbers(struct ListNode* l1, struct ListNode* l2) {
+    struct ListNode dummy;   // Dummy head node
+    dummy.next = NULL;
+    struct ListNode* tail = &dummy;
+
+    int carry = 0;
+
+    while (l1 != NULL || l2 != NULL || carry != 0) {
+        int sum = carry;
+
+        if (l1 != NULL) {
+            sum += l1->val;
+            l1 = l1->next;
+        }
+
+        if (l2 != NULL) {
+            sum += l2->val;
+            l2 = l2->next;
+        }
+
+        carry = sum / 10;          // Carry for next digit
+        int digit = sum % 10;      // Current digit
+
+        // Create new node
+        struct ListNode* newNode = (struct ListNode*)malloc(sizeof(struct ListNode));
+        newNode->val = digit;
+        newNode->next = NULL;
+
+        tail->next = newNode;
+        tail = newNode;
+    }
+
+    return dummy.next;
+}
+
